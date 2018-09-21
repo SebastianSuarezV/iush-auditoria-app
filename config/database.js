3801e17be98f36e79dcbf2f0f4c14ec3c8f6aead
Sebastian Suarez <sebastian.suarezv@comunidad.iush.edu.co>
2018-09-20 23:02:15 -0500

    Corrige error de usuario en la db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     "host": '104.248.189.81',
     "port": 3306,
-    "user": 'admin',
+    "user": 'auditoria-app',
     "acquireTimeout": 30000,
     "password": process.env.PASSWORD,
     "database": 'auditoria'
