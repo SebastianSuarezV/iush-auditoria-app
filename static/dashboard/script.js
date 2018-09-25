ac5e0ef3289dc5618c026570e9e1ee614a78f6f2
Sebastian Suarez <sebastian.suarezv@comunidad.iush.edu.co>
2018-09-25 14:08:36 -0500

    Agrega boton para ver las respuestas

--- static/dashboard/script.js
+++ static/dashboard/script.js
@@ -41,6 +41,7 @@ function generateTableContent(data) {
         });
         if(val.auditor != undefined) {
             rowContent += "<td><a class='btn btn-primary' href='https://iush-auditoria-app.herokuapp.com/resultados?auditoria=" + val.ID + "'>Ver resultados de la auditoria</a></td>";
+            rowContent += "<td><a class='btn btn-primary' href='https://iush-auditoria-app.herokuapp.com/respuestas?auditoria=" + val.ID + "'>Ver respuestas</a></td>";
         }
         
         
